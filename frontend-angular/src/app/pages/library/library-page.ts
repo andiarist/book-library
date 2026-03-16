2d06396de42c02779b40a3a50d18f2acdc19ddb0
agararis <andrea.garciaaristegui@emeal.nttdata.com>
2026-03-16 16:30:35 +0100

    init angular frontend

---
+++ frontend-angular/src/app/pages/library/library-page.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'library-page',
+  imports: [],
+  templateUrl: './library-page.html',
+})
+export default class LibraryPage {}
